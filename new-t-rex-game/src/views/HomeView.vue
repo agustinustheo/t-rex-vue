a6a085c8f7e3f6146b8ccd01cbd1f1c7fb32c1fa
Agustinus Theodorus <agustinustheoo@gmail.com>
2024-03-30 14:24:57 +0700

    feat: scaffold new t-rex vue project

---
+++ new-t-rex-game/src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <TrexGame />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import TrexGame from "@/components/TrexGame.vue"; // @ is an alias to /src
+
+export default defineComponent({
+  name: "TrexGame",
+  components: {
+    TrexGame,
+  },
+});
+</script>
